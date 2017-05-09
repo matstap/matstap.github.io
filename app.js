98378e0086af5ad640663af5a437e14bc5f32f33
Matthew Staples <staplma2@uw.edu>
2017-05-09 15:13:19 -0700

    Third question app.js

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ alert('Hi, there! Let\'s play a yes/no guessing game about Matthew.');
 
 var shoes = prompt('Question 1: Does Matthew own more than 5 pairs of shoes?');
 console.log('shoe guess:', shoes);
-var shoesUpper = shoes.toUpperCase();
+var shoesUp = shoes.toUpperCase();
 
 // give different alert for YES/Y, NO/N, and undefined answers
-if (shoesUpper === 'YES' || shoesUpper === 'Y') {
+if (shoesUp === 'YES' || shoesUp === 'Y') {
   alert('Ouch! Sorry, that\'s incorrect! He only has 4 pairs.');
-} else if (shoesUpper === 'NO' || shoesUpper === 'N') {
+} else if (shoesUp === 'NO' || shoesUp === 'N') {
   alert('Correct! Matthew has 3 pairs of shoes.');
 } else {
   alert('I can\'t understand you... Everything okay? Let\'s just move on.');
@@ -18,14 +18,26 @@ if (shoesUpper === 'YES' || shoesUpper === 'Y') {
 // add a maybe case
 var married = prompt('Question 2: Is Matthew married?');
 console.log('married guess', married);
-var marriedUpper = married.toUpperCase();
+var marriedUp = married.toUpperCase();
 
-if (marriedUpper === 'Y' || marriedUpper === 'YES') {
+if (marriedUp === 'Y' || marriedUp === 'YES') {
   alert('Correct! Matthew has been married for a year come June.');
-} else if ( marriedUpper === 'N' || marriedUpper === 'NO') {
+} else if ( marriedUp === 'N' || marriedUp === 'NO') {
   alert('Wrong! Matthew is married.');
-} else if (marriedUpper === 'MAYBE') {
+} else if (marriedUp === 'MAYBE') {
   alert('No cheating! Now you will never know!') ;
 } else {
   alert('Not an answer. Let\'s move on.');
+}
+
+var zebra = prompt('Question 3: Is Matthew a Zebra?');
+console.log('zebra guess:', zebra);
+var zebraUp = zebra.toUpperCase();
+
+if (zebraUp === 'Y' || zebraUp === 'YES') {
+  alert('Neither correct or incorrect. Zebra is a state of mind, friend.');
+} else if (zebraUp === 'N' || zebraUp === 'NO') {
+  alert('Hey, man. You never know.');
+} else {
+  alert('Incorrect answer type, friend! Let\'s move on.');
 }
