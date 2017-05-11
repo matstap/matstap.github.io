8beab01c06d4b31252495314c2e6d29330a00ec7
taxpayer131 <chris.stefan1844@gmail.com>
2017-05-11 16:53:27 -0700

    one through five quest fxn

--- app.js
+++ app.js
@@ -6,6 +6,59 @@
 alert('Hi, there! Let\'s play a yes/no guessing game about Matthew.');
 var rightAnswers = 0;
 
+var entries = [
+  'Question 1: Does Matthew own more than 5 pairs of shoes?',
+  'Question 2: Is Matthew married?',
+  'Question 3: Is Matthew a Zebra?',
+  'Question 4: Does Matthew like potato chips?',
+  'Question 5: Is Eric Andre Matthew\'s favorite comedian?',
+  'Question 6: What is Matthew\'s favorite number between 0 and 20?',
+  'Question 7: What is one of Matthew\'s hobbies?'
+];
+
+var correct = [
+  'N',
+  'Y',
+  'Y',
+  'Y',
+  'N',
+  Math.floor(Math.random() * 21),
+  ['video games', 'hiking', 'cooking', 'saxophone']
+];
+
+var responses = [
+  'He only has 4 pairs.',
+  'Matthew has been married for a year come June.',
+  'Neither correct or incorrect. Zebra is a state of mind, friend.',
+  'Matthew has an unhealthy love of potato chips!',
+  'He\'s still funny though. Bird Up!!!'
+]
+
+function quest15(entry, correct, response) {
+  var answer = prompt(entry).toUpperCase();
+  console.log('guess:', answer);
+
+  // give different alert for YES/Y, NO/N, and undefined answers
+  // while loops to make sure of valid answers
+  while (answer!== 'YES' && answer !== 'Y' && answer !== 'N' && answer !== 'NO') {
+    alert('Let\'s try that again.');
+    answer = prompt(entry).toUpperCase();
+    console.log('answer guess:', answer);
+  }
+
+  if (answer.charAt[0] !== correct) {
+    alert('Ouch! Sorry, that\'s incorrect! ' + response);
+    return 0;
+  } else {
+    alert('Correct! ' + response);
+    return 1;
+  }
+}
+
+for (var i=0; i < entries.length; i++) {
+  rightAnswers += quest15(entries[i], correct[i], responses[i]);
+}
+
 // question 1
 var shoes = prompt('Question 1: Does Matthew own more than 5 pairs of shoes?').toUpperCase();
 console.log('shoe guess:', shoes);
